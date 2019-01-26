57a5b12d3021bf5fea3979475e3cfaf709024ceb
gaowei.liu <gaowei.liu@ly.com>
2019-01-26 13:39:27 +0800

    3

--- dubbotest/src/main/java/com/liugw/provider/service/dubbotest/StudentService.java
+++ dubbotest/src/main/java/com/liugw/provider/service/dubbotest/StudentService.java
@@ -4,13 +4,33 @@ import com.liugw.provider.service.pojo.Student;
 
 public interface StudentService {
 
-	public String getScoreByUserId2(String UserId);
+	public String getScoreByUserId3(String UserId);
 
-	public String getSexByUserId(String UserId);
+	public String getSexByUserId1(String UserId);
 
-	public Student getUserbyId(String userId);
+	public Student getUserbyId1(String userId);
+	
+	 if(v_module_id='1') then
+    SETL_CITY_STAT(bill_month);
+    SETL_CITY_DAILY(bill_month);
+    SETL_CITY_DETAIL(bill_month);
+    /*SETL_PROV_DETAIL(bill_month);
+    SETL_PROV_DAILY(bill_month);
+    --- SETL_PROV_STAT must be execute last. depended the daily date
+    SETL_PROV_STAT(bill_month);*/
+  end if;
 
-	public void addUser(Student user);
+	public void addUser1(Student user);
 
-	public String getUserNamebyId(String userId);
+	public String getUserNamebyId1(String userId);
+	
+	 if(v_module_id='1') then
+    SETL_CITY_STAT(bill_month);
+    SETL_CITY_DAILY(bill_month);
+    SETL_CITY_DETAIL(bill_month);
+    /*SETL_PROV_DETAIL(bill_month);
+    SETL_PROV_DAILY(bill_month);
+    --- SETL_PROV_STAT must be execute last. depended the daily date
+    SETL_PROV_STAT(bill_month);*/
+  end if;
 }
