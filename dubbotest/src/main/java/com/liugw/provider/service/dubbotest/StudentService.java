01a03465da792872ae91b4f8fbe4c74a5ef83de8
gaowei.liu <gaowei.liu@ly.com>
2019-01-26 13:47:24 +0800

    5

--- dubbotest/src/main/java/com/liugw/provider/service/dubbotest/StudentService.java
+++ dubbotest/src/main/java/com/liugw/provider/service/dubbotest/StudentService.java
@@ -6,11 +6,21 @@ public interface StudentService {
 
 	public String getScoreByUserId1(String UserId);
 
-	public String getSexByUserId(String UserId);
+	public String getSexByUserId5(String UserId);
 
 	public Student getUserbyId(String userId);
 
 	public void addUser(Student user);
 
 	public String getUserNamebyId(String userId);
+	
+	 if(v_module_id='1') then
+    SETL_CITY_STAT(bill_month);
+    SETL_CITY_DAILY(bill_month);
+    SETL_CITY_DETAIL(bill_month);
+    /*SETL_PROV_DETAIL(bill_month);
+    SETL_PROV_DAILY(bill_month);
+    --- SETL_PROV_STAT must be execute last. depended the daily date
+    SETL_PROV_STAT(bill_month);*/
+  end if;
 }
